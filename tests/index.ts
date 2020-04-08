9c2ea1d61fd068c55042562ae7dd447fcf362f23
Dan Minshew <ofenixculpa@gmail.com>
2020-04-07 22:06:43 -0500

    update tests

--- tests/index.ts
+++ tests/index.ts
@@ -1,7 +1,8 @@
 import axios from 'axios'
 import MockAdapter from 'axios-mock-adapter'
+
 import { buildIndexUrl, buildSheetUrl } from '../src'
-import { mockWorksheet, mockSheet } from './__mocks__/'
+import { mockWorksheet, mockSheet } from './__mocks__'
 
 const mock = new MockAdapter(axios)
 const key = 'exampleKey'
@@ -11,3 +12,11 @@ const sheetUrl = buildSheetUrl(key, 'sheetId1')
 
 mock.onGet(worksheetUrl).reply(200, mockWorksheet)
 mock.onGet(sheetUrl).reply(200, mockSheet)
+
+test('getData', () => {
+  it('should return processed data', () => {
+    axios.get(worksheetUrl).then((d) => {
+      expect(d.data).toEqual(mockWorksheet)
+    })
+  })
+})
